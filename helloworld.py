88aee7ab97143a57d9111db745c76c65aec54d30
miwilc <58687435+miwilc@users.noreply.github.com>
2019-12-09 11:31:34 +0100

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,19 @@
-
+class Person():
+    def __init__(self,name,lastname):
+        self.name = name
+        self.lastname = lastname
+        
+    def __repr__(self):
+        return f"{self.name} {self.lastname}"
+    
+    @classmethod
+    def Person_fromstring(cls,string):
+        return cls(string.split(" ")[0],string.split(" ")[1])
+    
+class Student(Person):
+     def __init__(self, name,lastname,programme):
+        super().__init__(name,lastname)
+        self.programme = programme
+    
+     def __repr__(self):
+        return super().__repr__() + f" {self.programme}"
